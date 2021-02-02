1291c2f442e1c58b4cc933353c4ba3358879bfad
Oisín_windows <byrne.ois@gmail.com>
2021-02-02 11:47:17 +0000

    need to mkae responsive next

--- src/components/about.js
+++ src/components/about.js
@@ -1,6 +1,6 @@
 const About = () => {
 
-    return ( 
+    return (
         
         <div className='about-container'>
             <div className='about-images-container'>
@@ -15,6 +15,7 @@ const About = () => {
                 </div>
             </div>
             <div className='about-info-container'>
+                <div className='div-spacer'></div>
                 <div className='about-info'>
                     <h2> About Me </h2>
                     <p> Laborum laborum laborum esse aliqua occaecat non culpa ea Lorem proident cillum. Cillum sunt voluptate magna eiusmod laboris laboris ut nisi non et nisi. Reprehenderit voluptate deserunt ad eu eiusmod eiusmod. Laborum laborum laborum esse aliqua occaecat non culpa ea Lorem proident cillum. Cillum sunt voluptate magna eiusmod laboris laboris ut nisi non et nisi. Reprehenderit voluptate deserunt ad eu eiusmod eiusmod. </p>
@@ -27,6 +28,11 @@ const About = () => {
                     <h2> My Coaching Style </h2>
                     <p> Laborum laborum laborum esse aliqua occaecat non culpa ea Lorem proident cillum. Cillum sunt voluptate magna eiusmod laboris laboris ut nisi non et nisi. Reprehenderit voluptate deserunt ad eu eiusmod eiusmod. Laborum laborum laborum esse aliqua occaecat non culpa ea Lorem proident cillum. Cillum sunt voluptate magna eiusmod laboris laboris ut nisi non et nisi. Reprehenderit voluptate deserunt ad eu eiusmod eiusmod. </p>
                 </div>
+            </div>
+            <div className='booking-btn-container'>
+                <button className='booking-btn'>
+                    Book a complimentary coaching session with Mona
+                </button>
             </div>
         </div>
 
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,22 +1,30 @@
 const Sidebar = () => {
 
+
     const listReveal = () =>{
 
+        // grabs all the links below potential sub-link list
         let moveableList = document.getElementById('moveable-list')
 
+        // slides sub-links in from the left
         let links = document.getElementById('coaching-links')
-        if(links.style.opacity === '0'){
+        console.log(links.style.opacity);
+        // '' logical condition for the first time the function is fired
+        if(links.style.opacity === '' || links.style.opacity === '0'){
             links.style.opacity = 1
             links.style.left = '0%'
-            links.style.transition = '1.5s'
+            links.style.transition = '1.2s'
+            // moves standard links down 
             moveableList.style.top = '0%'
             return
         }
         else{
+            // moves sub-links back left, off-screen
             links.style.opacity = 0
             links.style.left = '-100%'
             links.style.transition = '0.5s'
-            moveableList.style.top = '-42%'
+            // moves standard links back up
+            moveableList.style.top = '-47%'
         }
 
     }
@@ -27,12 +35,12 @@ const Sidebar = () => {
             <div className="sidebar-content">
                 <div className="side-brand side">
                     <p>Mona Eames</p>
-                    <p>The Sounding Board</p>
+                    <img src="/imgs/brand.png" alt=""/>
                 </div>
                 <div className="side-links side">
                     <ul className="side-links-list">
-                        <li>About</li>
-                        <li onClick={listReveal}>Coaching</li>
+                        <li className='about-link'>About</li>
+                        <li className='coaching-link' onClick={listReveal}>Coaching</li>
                         <ul className="coaching-links" id ='coaching-links'>
                             <li> - Executive</li>
                             <li> - Lifestyle</li>
@@ -53,14 +61,15 @@ const Sidebar = () => {
                 </div>
                 <div className="side-icons side">
                     <span className='side-icon-container'>
-                        <img src="/icons/fb.svg" alt=""/>
+                        <img src="/icons/fb2.png" alt=""/>
+                     </span>
+                     <span className='side-icon-container'>
+                        <img src="/icons/in2.png" alt=""/>
                     </span>
                     <span className='side-icon-container'>
-                        <img src="/icons/mail.svg" alt=""/>
+                        <img src="/icons/mail2.png" alt=""/>
                     </span>
-                    <span className='side-icon-container'>
-                        <img src="/icons/fb.svg" alt=""/>
-                    </span>
+
                 </div>
             </div>
         </div>
