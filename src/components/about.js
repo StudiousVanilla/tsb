c519932cde440ccc4e7a3f1e8cc571eba1645bc2
Oisín_windows <byrne.ois@gmail.com>
2021-02-09 16:38:51 +0000

    finished 1st draft of about page

--- src/components/about.js
+++ src/components/about.js
@@ -5,24 +5,24 @@ const About = () => {
 
     return (
         
-        <div className='about-container'>
+        <div className='about-container content-container'>
             <div className='about-images-container'>
                 <div className='about-image-container'>
                     <img src={placeholderImg} alt='book' className='about-img'/>
                 </div>
             </div>
             <div className='about-info-container'>
-                <div className='div-spacer'></div>
                 <div className='about-info'>
-                    <h2> About Me </h2>
+                    <h2 className='about-head'> About Me </h2>
                     <p className='about-content'> Laborum laborum laborum esse aliqua occaecat non culpa ea Lorem proident cillum. Cillum sunt voluptate magna eiusmod laboris laboris ut nisi non et nisi. Reprehenderit voluptate deserunt ad eu eiusmod eiusmod. Laborum laborum laborum esse aliqua occaecat non culpa ea Lorem proident cillum. Cillum sunt voluptate magna eiusmod laboris laboris ut nisi non et nisi. Reprehenderit voluptate deserunt ad eu eiusmod eiusmod. </p>
                 </div>
                 <div className='about-info'>
-                    <h2> About The Sounding Board </h2>
+                    <h2 className='about-head'> About The Sounding Board </h2>
                     <p className='about-content'> Laborum laborum laborum esse aliqua occaecat non culpa ea Lorem proident cillum. Cillum sunt voluptate magna eiusmod laboris laboris ut nisi non et nisi. Reprehenderit voluptate deserunt ad eu eiusmod eiusmod. Laborum laborum laborum esse aliqua occaecat non culpa ea Lorem proident cillum. Cillum sunt voluptate magna eiusmod laboris laboris ut nisi non et nisi. Reprehenderit voluptate deserunt ad eu eiusmod eiusmod. </p>
                 </div>
-                <div className='about-info'>
-                    <h2> My Coaching Style </h2>
+                {/* Inline style of border is becuase this is the last paragrpah and so has no bottom border */}
+                <div className='about-info' style={{border: "none"}}>
+                    <h2 className='about-head'> My Coaching Style </h2>
                     <p className='about-content'> Laborum laborum laborum esse aliqua occaecat non culpa ea Lorem proident cillum. Cillum sunt voluptate magna eiusmod laboris laboris ut nisi non et nisi. Reprehenderit voluptate deserunt ad eu eiusmod eiusmod. Laborum laborum laborum esse aliqua occaecat non culpa ea Lorem proident cillum. Cillum sunt voluptate magna eiusmod laboris laboris ut nisi non et nisi. Reprehenderit voluptate deserunt ad eu eiusmod eiusmod. </p>
                 </div>
             </div>
